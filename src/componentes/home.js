a615d5d39a2849d0f7e01b359dcf0dd94e1a27b8
Gadd88 <matiassaade@gmail.com>
2022-07-09 04:18:37 -0300

    starting a simple calculator project

---
+++ src/componentes/home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./home.css"
+
+const home = () => {
+  return (
+    <div className='home'>
+        <div className='inner'>
+            <div className='result'>
+                <div className='resbox'></div>
+            </div>
+            <div className='btns'>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default home
